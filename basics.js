1a288ebe157415fdfbe72b9b11d14a109502d2b1
Binita rijal <rijalbinita01@gmail.com>
2025-02-05 12:58:50 +0545

    js datatypes

---
+++ basics.js
@@ -0,0 +1,19 @@
+let a="wow";
+var b=1234;
+const c="12345";
+d="hehe";
+a="lol";
+d="jjjj";
+let y;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+console.table([a,b,c]);
+console.log(y);
+console.log(a + b);
+
+//prefer not to use var 
+console.log("peps");
+console.log("uffss");
--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+"use strict";
+//alert("hii");   we are using node js not browser
 console.log("hehe");
+console.log("k chaa");
+var a=112;
+let b="k";
+let c="jdshaj";
+let d;
+var e=null;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
