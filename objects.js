5627a5c4ec84d8f9df3ea03de03ce95943192b24
Binita rijal <rijalbinita01@gmail.com>
2025-02-07 11:55:12 +0545

    objects

---
+++ objects.js
@@ -0,0 +1,28 @@
+const a={
+    name:"binita",
+    surename:"rijal",
+    age:20,
+    classs:14,
+    course:{
+        sub1:"dbms",
+        sub2:"Os",
+        sub3:"Oop",
+    }
+}
+a.greeting=function(){
+    console.log("hi cutiee!!!");
+
+}
+a.hello=function(){
+    console.log(this.classs);
+}
+a.greeting();
+a.hello();
+ //console.log(a);
+ //console.log(a["name"]);
+ //console.log(a.age);
+
+ a.age=21;
+//  console.log(a["age"]);
+//  console.log(a.course.sub1)
+//  console.log(a.course)
