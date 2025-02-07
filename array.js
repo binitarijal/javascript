45b088c9558c54839ee0c09747ebc1ad2e4a8640
Binita rijal <rijalbinita01@gmail.com>
2025-02-07 09:47:54 +0545

    array more

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 let a=[1,2,3,4];
-let b= new Array(4,5,6,7);
+let b= new Array(4,5,6,7,"bini",true);
 // console.log(a[1]);
 // console.log(a[0]);
 // console.log(a[3]);
@@ -12,15 +12,40 @@ let b= new Array(4,5,6,7);
 
 a.push(9);
 a.push(11);
-console.log(a);
+//console.log(a);
 a.pop();
-console.log(a);
+//console.log(a);
 
 a.unshift(4);
-console.log(a);
+//console.log(a);
 
 a.shift();
-console.log(a);
+//console.log(a);
 
-console.log(b.indexOf(5))
-console.log(a.indexOf(3))
+//console.log(b.indexOf(5))
+//console.log(a.indexOf(3))
+
+//slice and spice
+
+const c=a.slice(0,2);
+//console.log(c);
+//console.log(a);
+const d=a.splice(0,2); // it changes original array
+//console.log(d);
+//console.log(a);
+
+//combining two arrays
+const e=[...a,...b];  //spread operator
+//console.log(e);
+
+//array inside array
+
+const arrr=[12,[4,5,6],7,8,[1,5,6,[8,9,3]]];
+const arr1=arrr.flat(Infinity);
+console.log(arr1);
+
+console.log(Array.from("BINITA"))
+console.log(Array.from(Object.values({name:"rijal"}))) //interesting
+console.log(Array.from(Object.keys({name:"rijal"}))) //interesting
+
+
