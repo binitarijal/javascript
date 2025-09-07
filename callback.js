331aef4bb88dd7edcae7b8d3918c733f00acc785
Binita rijal <rijalbinita01@gmail.com>
2025-09-07 22:17:16 +0545

    3rd callback

--- callback.js
+++ callback.js
@@ -10,12 +10,29 @@
 
 // greet("John", afterGreeting);
 
-// 2. Anonymous Callback
-greet("Sarah", function() {
-    console.log("How are you doing?");
+// // 2. Anonymous Callback
+// greet("Sarah", function() {
+//     console.log("How are you doing?");
+// });
+
+// // 3. Arrow Function Callback
+// greet("Mike", () => {
+//     console.log("Have a great day!");
+// });
+
+
+// 4. Array Methods with Callbacks
+const numbers = [1, 2, 3, 4, 5];
+
+// forEach callback
+numbers.forEach(function(num, index) {
+    console.log(`Index ${index}: ${num}`);
 });
 
-// 3. Arrow Function Callback
-greet("Mike", () => {
-    console.log("Have a great day!");
-});
+// map callback
+const doubled = numbers.map(num => num * 2);
+console.log(doubled); // [2, 4, 6, 8, 10]
+
+// filter callback
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers); // [2, 4]
