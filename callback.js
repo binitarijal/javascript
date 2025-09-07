2d3338ad9b61a3f5c8f7a08a8e12b14e431bfe86
Binita rijal <rijalbinita01@gmail.com>
2025-09-07 22:16:10 +0545

    2 callback

--- callback.js
+++ callback.js
@@ -1,11 +1,21 @@
-// 1. Basic Callback Function
-function greet(name, callback) {
-    console.log(`Hello ${name}!`);
-    callback();
-}
+// // 1. Basic Callback Function
+// function greet(name, callback) {
+//     console.log(`Hello ${name}!`);
+//     callback();
+// }
 
-function afterGreeting() {
-    console.log("Nice to meet you!");
-}
+// function afterGreeting() {
+//     console.log("Nice to meet you!");
+// }
 
-greet("John", afterGreeting);
+// greet("John", afterGreeting);
+
+// 2. Anonymous Callback
+greet("Sarah", function() {
+    console.log("How are you doing?");
+});
+
+// 3. Arrow Function Callback
+greet("Mike", () => {
+    console.log("Have a great day!");
+});
